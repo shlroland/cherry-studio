f1ce7ca2545c690bc4242a6490d7100591a45bcb
shlroland <shlroland1995@gmail.com>
2025-03-17 16:23:10 +0800

    feat(nutstore): add nutstore intergration

--- src/renderer/src/store/settings.ts
+++ src/renderer/src/store/settings.ts
@@ -1,8 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { TRANSLATE_PROMPT } from '@renderer/config/prompts'
 import { CodeStyleVarious, LanguageVarious, ThemeMode, TranslateLanguageVarious } from '@renderer/types'
-
-import { WebDAVSyncState } from './runtime'
+import { WebDAVSyncState } from './backup'
 
 export type SendMessageShortcut = 'Enter' | 'Shift+Enter' | 'Ctrl+Enter' | 'Command+Enter'
 
